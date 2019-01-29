84a0e6a5903d477bcf4e46a295e926382e80b9a2
Thavin Voeun <voeunthavin17@kit.edu.kh>
2019-01-29 19:01:03 +0700

    Update profile.hh
    Add more comments

--- profile.hh
+++ profile.hh
@@ -19,29 +19,30 @@ public:
   }
   
   // distance func take no argument & return m_distance as float of the object
-  float distance(void) { return m_distance; }
+  float getDistance(void) { return m_distance; }
   
   // duration func take no argument & return m_duration as float of the object
-  float duration(void) { return m_duration; }
+  float getDuration(void) { return m_duration; }
   
   // empty func take no argument & return if m_duration == 0 or not
-  bool empty(void) { return m_duration == 0; }
+  bool ifEmpty(void) { return m_duration == 0; }
   
   // value func take a float arugment "time"  
   float value(float time) { // The core func of this module calculating "time" with the object's fields - "m_distance" & "m_duration"
     float retval;
     if (time > 0) // If "time" is bigger than 0 then
       if (time < m_duration) // If "time" is smaller then 
-        // 
+        // ???
         retval = m_distance * (M_PI * time / m_duration - 0.5 * sin(2 * M_PI * time / m_duration)) / M_PI;
       else
-        retval = m_distance;
+        retval = m_distance; // IF "time" < 0 "retval" = "m_distance"
     else
       retval = 0;
     return retval;
   }
+  // timeRequired func takes 2 float arguments: "distance" & " maxJerk"
   static float timeRequired(float distance, float maxJerk) {
-    return cbrtf(4 * M_PI * M_PI * distance / maxJerk);
+    return cbrtf(4 * M_PI * M_PI * distance / maxJerk); // ???
   }
   
 protected:
