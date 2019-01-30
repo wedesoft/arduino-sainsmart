3e5c10fddcf54f72ee92fb7eb7e90cd32891069c
Thavin Voeun <voeunthavin17@kit.edu.kh>
2019-01-30 12:34:43 +0700

    Update path.hh
    Add additional changes

--- path.hh
+++ path.hh
@@ -13,20 +13,25 @@ public:
     m_time[1] = 0;
   }
   
-  // "pos" func take no argument and yield the sum of "offset" with the "retval" value from "rofile" class
+  // "pos" func take no argument and yield the sum of "offset" with the "retval" value from "m_profile" object
   float pos(void) {
     return m_offset + m_profile[0].value(m_time[0]) + m_profile[1].value(m_time[1]);
   }
+  
+  // "update" func that returns float take a float argument & call "update" func that take 3 arguments below
   float update(float dt) {
     update(dt, m_time[0], m_profile[0]);
     update(dt, m_time[1], m_profile[1]);
-    return pos();
+    return pos(); // Return the float value from "pos(void)" func
   }
+  
+  // "update" func with no return type & take 3 arguments
   void update(float dt, float &time, Profile &profile) {
     time += dt;
-    if (time >= profile.duration()) {
-      m_offset += profile.distance();
-      profile.reset();
+    // Check "time" is larger or equals to the duration of profile object
+    if (time >= profile.getDuration()) {
+      m_offset += profile.getDistance(); // Sum all the distance from profile object
+      profile.reset(); // Then give default value to profile object variables
     };
   }
   void stop(float pos) {
@@ -35,16 +40,16 @@ public:
     m_profile[1].reset();
   }
   bool ready(void) {
-    return m_profile[0].empty() || m_profile[1].empty();
+    return m_profile[0].ifEmpty() || m_profile[1].ifEmpty();
   }
   void retarget(float target, float duration) {
     retarget(target, duration, m_time[0], m_profile[0]) || retarget(target, duration, m_time[1], m_profile[1]);
   }
   float target(void) {
-    return m_offset + m_profile[0].distance() + m_profile[1].distance();
+    return m_offset + m_profile[0].getDistance() + m_profile[1].getDistance();
   }
   bool retarget(float value, float duration, float &time, Profile &profile) {
-    if (!profile.empty())
+    if (!profile.ifEmpty())
       return false;
     else {
       time = 0;
@@ -53,7 +58,7 @@ public:
     };
   }
   float timeRemaining(float time, Profile &profile) {
-    return profile.empty() ? 0 : profile.duration() - time;
+    return profile.ifEmpty() ? 0 : profile.getDuration() - time;
   }
   float timeRemaining(void) {
     float a = timeRemaining(m_time[0], m_profile[0]);
