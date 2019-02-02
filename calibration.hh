61222482e136b6ddcf53c4516d250a6754eba96d
voeuthavin <voeunthavin17@kit.edu.kh>
2019-02-02 10:05:23 +0700

    Update the min and max pulse width of elbow

--- calibration.hh
+++ calibration.hh
@@ -9,8 +9,8 @@ const int OFFSET[] = {1471, 1465, 700, 1240, 1550, 1366};// normally around 1500
 
 #warning "Adjust these values for your robot to avoid self-collisions!"
 #warning "Control your robot carefully until you have inserted appropriate boundaries here!"
-const int MIN[] = {544, 938, 638, 544, 544, 544};// must not be below 544
-const int MAX[] = {2400, 2400, 2400, 2400, 2253, 2400};// must not be above 2400
+const int MIN[] = {544, 938, 500, 544, 544, 544};// must not be below 544
+const int MAX[] = {2400, 2400, 2000, 2400, 2253, 2400};// must not be above 2400
 
 const float RESOLUTION[] = {8.844444444444445, 8.844444444444445, 8.844444444444445, 8.844444444444445, 11.433333333333334, 10.133333333333333};
 const float MAXJERK = 250;
